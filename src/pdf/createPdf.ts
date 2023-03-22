d11c8e70d3ec6d9500570a2c601453e7bd2f2cad
Dave Garnar <dgarnar@gmail.com>
2023-03-22 00:18:16 +0000

    adding basic template and rendering pdf from template

--- src/pdf/createPdf.ts
+++ src/pdf/createPdf.ts
@@ -1,10 +1,16 @@
 import * as puppeteer from 'puppeteer';
+import * as handlebars from 'handlebars';
+import fs from 'fs';
 
 export default class PDFFunctions {
     public async createPdf(): Promise<Buffer> {
+        const templateString = fs.readFileSync('src/pdf/template/script.handlebars').toString('utf-8')
+        const template = handlebars.compile(templateString);
+
+
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto('https://google.com');
+        await page.setContent(template({helloWorld: "Hello World"}));
         const pdf = await page.pdf();
         await browser.close();
 
