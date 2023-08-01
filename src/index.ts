57edb75b198b7a8842a4a3eca75ddd0cb62f5a4b
Dave <d.garnar@uk.cdw.com>
2023-08-01 11:55:06 +0100

    fixing port conflict

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(
     }),
 );
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 app.listen(port, () => console.log(`App listening on PORT ${port}`));
 
 const tempPath = path.resolve(__dirname, './temp');
