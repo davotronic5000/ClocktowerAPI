44588b4fe5a5d107c91787091d72b01693f29bef
Dave <d.garnar@uk.cdw.com>
2023-08-02 14:55:59 +0100

    fixing css error?

--- src/pdfProcessor.ts
+++ src/pdfProcessor.ts
@@ -35,13 +35,13 @@ export default class PdfProcessor {
         const template = handlebars.compile(templateString);
 
         fs.writeFile(templatePath, template(scriptData), () => {});
-        const browser = await puppeteer.launch({ headless: 'new' });
+        const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
         await page.goto('file:///' + path.resolve(__dirname, templatePath), {
             waitUntil: 'networkidle0',
         });
         const pdf = await page.pdf({ format: 'A4', printBackground: true });
-        await browser.close();
+        //await browser.close();
         return pdf;
     }
 }
