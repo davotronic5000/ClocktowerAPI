ee3b0618184f30dcc259a06fedb715d9109128c1
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-09-01 16:09:25 +0100

    process roles into better roles

---
+++ src/roles-processing.js
@@ -0,0 +1,29 @@
+var roleData = require('./data/roles.json');
+var fs = require('fs');
+
+const processor = (roles) => {
+    return roles.map((role) => ({
+        id: role.id,
+        name: role.name,
+        team: role.team,
+        firstNight: role.firstNight,
+        firstNightReminder: role.firstNightReminder,
+        otherNight: role.otherNight,
+        otherNightReminder: role.otherNightReminder,
+        reminders: [...new Set(role.reminders.concat(role.remindersGlobal))]
+            .filter((role) => role)
+            .map((reminder) => ({ text: reminder, count: 1 })),
+        setup: role.setup,
+        ability: role.ability,
+        image: role.image,
+        count: 1,
+    }));
+};
+
+fs.writeFile(
+    'src/data/roles-new.json',
+    JSON.stringify(processor(roleData)),
+    (err) => {
+        console.log(err);
+    },
+);
